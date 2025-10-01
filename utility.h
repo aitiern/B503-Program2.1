dae7abcb5be9f3cbd699d9541abbc7dcdac086d4
aitiern <aitiern@iu.edu>
2025-10-01 12:10:17 -0400

    Update utility.h

---
+++ utility.h
@@ -0,0 +1,21 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+// ANSI/standard includes that are commonly handy in CS coursework
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+#endif // UTILITY_H
